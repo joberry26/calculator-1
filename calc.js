25aaf12e634c12a79084a90fd13afae39a7470f7
Joseph Berry <josephberry910@gmail.com>
2021-09-07 14:35:24 -0600

    Inital commit 2.0

--- calc.js
+++ calc.js
@@ -6,16 +6,21 @@ const reader = readline.createInterface({
 });
 
 
-
 reader.question("What would you like to calculate?", function(input){
 	tokens = input.split(' ');
-	
+
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
+	console.log('mathSymbol', mathSymbol);
+	console.log('num1', num1);
+	console.log('num2', num2);
 
-	// This line closes the connection to the command line interface.
+	if (mathSymbol === '+'){
+		console.log(num1 + num2);
+	}
+
 	reader.close()
 
 });
